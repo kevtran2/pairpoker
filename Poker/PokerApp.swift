7b1ab97917ebdb84322bb526ec6eeba98c07aa66
Kevin Tran <kevin.tran.675@gmail.com>
2024-08-29 22:13:16 -0700

    Initial Commit

---
+++ Poker/PokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerApp.swift
+//  Poker
+//
+//  Created by Kevin Tran on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
